e901ecd6d27e791e1a15bda3b6ace9f53392683f
Jesus Borrero <jesufer2018@hotmail.com>
2026-02-26 18:28:27 -0500

    feat: add component to upload images

---
+++ components/shared/image-upload.tsx
@@ -0,0 +1,29 @@
+import { useTransition } from "react"
+import { Camera } from "lucide-react"
+
+interface ImageUploadProps {
+  onFileChange: (file: File) => void
+}
+
+export function ImageUpload({ onFileChange }: ImageUploadProps) {
+  const [isPending, startTransition] = useTransition()
+
+  function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0]
+    if (file) startTransition(async () => onFileChange(file))
+  }
+
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center size-10 bg-muted/80 opacity-0 group-hover:opacity-100 transition-opacity rounded-full overflow-hidden backdrop-blur">
+      <input
+        type="file"
+        multiple={false}
+        accept="image/png, image/jpeg, image/jpg"
+        className="w-10 h-10 opacity-0 z-10 cursor-pointer file:cursor-pointer"
+        disabled={isPending}
+        onChange={handleFileChange}
+      />
+      <Camera className="absolute text-foreground/80" />
+    </div>
+  )
+}
